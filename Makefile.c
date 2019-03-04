c098722f8a41d1a23f298d98d2fe396ae16d7df4
Парфенцов Семён Анатольевич <ip814s19@mwp04.csc.local>
2019-03-04 14:50:53 +0700

    Popitka nomber 1

--- Makefile.c
+++ Makefile.c
@@ -1,11 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <graphics>
 
 int main()
 {
-    initwindow(600,400);
-    circle (20,20,10);
-    closegraph();
+    int N1,N2,N3,R,K;
+	K=0;
+	printf ("Vvedite N1");
+	scanf ("%d",&N1);
+	K++;
+	printf ("Vvedite N2");
+	scanf ("%d",&N2);
+	K++;
+	printf ("Vvedite N3 or Vvedite 0");
+	scanf ("%d",&N3);
+	if (N3 > 0)
+       	K++;
+	printf ("Vvedite R");
+	scanf ("%d",&R);
+	if (R == 0)
+		printf ("It's not circle!");
+	else 
+		printf ("wait pliase");
+	if (K > 2)
+		printf ("It's Triangle");
+	else 
+		printf ("It's Circle");
+	return 0;
+	system ("PAUSE"); 
 }
